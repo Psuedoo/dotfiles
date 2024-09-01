c2c08b1b1f907d08d18affac328bc06ece383402
Psuedoo <forthewin720@gmail.com>
2024-09-01 03:08:25 -0400

    fix wallpaper stuff

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -4,6 +4,5 @@ wallpaper=$(cat ~/.cache/wal/colors.json | jq '.wallpaper' | sed 's/"//g')
 feh --bg-scale $wallpaper &
 
 alacritty &
-# wal -b 282738 -i ~/.config/Wallpaper/120_-_KnFPX73.jpg &&
 picom --config ~/.config/picom/picom.conf &
 
